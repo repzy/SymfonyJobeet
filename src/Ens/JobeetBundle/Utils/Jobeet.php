9ca937d2ba37bdd9add346562b374b7e0c738132
repzy <rrepzy@gmail.com>
2014-12-03 23:46:41 +0200

    change routing

---
+++ src/Ens/JobeetBundle/Utils/Jobeet.php
@@ -0,0 +1,15 @@
+<?php
+namespace Ens\JobeetBundle\Utils;
+
+class Jobeet
+{
+    static public function slugify($text)
+    {
+        $text = preg_replace('/\W+/', '-', $text);
+
+        $text = strtolower(trim($text, '-'));
+
+        return $text;
+    }
+}
+?>
