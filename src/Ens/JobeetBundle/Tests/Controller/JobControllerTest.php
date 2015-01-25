07b304e761615756704d8819f2011bc031228b7b
repzy <rrepzy@gmail.com>
2015-01-25 02:14:37 +0200

    test JobController

--- src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ens/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Ens\JobeetBundle\Tests\Controller;
 
+use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Output\NullOutput;
@@ -14,4 +15,65 @@ class JobControllerTest extends WebTestCase
 {
     private $em;
     private $application;
+
+    public function setUp()
+    {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+
+        $this->application = new Application(static::$kernel);
+
+        //drop the database
+        $command = new DropDatabaseDoctrineCommand();
+        $this->application->add($command);
+        $input = new ArrayInput(array(
+            'command' => 'doctrine:database:drop',
+            '--force' => true
+        ));
+        $command->run($input, new NullOutput());
+
+        //we have to close the connection after dropping the database so we dont get "No database selected" error
+        $connection = $this->application->getKernel()->getContainer()->get('doctrine')->getConnection();
+        if($connection->isConnected()) {
+            $connection->close();
+        }
+
+        //create the database
+        $command = new CreateDatabaseDoctrineCommand();
+        $this->application->add($command);
+        $input = new ArrayInput(array(
+            'command' => 'doctrine:database:create'
+        ));
+        $command->run($input, new NullOutput());
+
+        //create schema
+        $command = new CreateSchemaDoctrineCommand();
+        $this->application->add($command);
+        $input = new ArrayInput(array(
+            'command' => 'doctrine:schema:create'
+        ));
+        $command->run($input, new NullOutput());
+
+        //get Entity Manager
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        //load Fixtures
+        $client = static::createClient();
+        $loader = new \Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader($client->getContainer());
+        $loader->loadFromDirectory(static::$kernel->locateResource('@EnsJobeetBundle/DataFixtures/ORM'));
+        $purger = new \Doctrine\Common\DataFixtures\Purger\ORMPurger($this->em);
+        $executor = new \Doctrine\Common\DataFixtures\Executor\ORMExecutor($this->em, $purger);
+        $executor->execute($loader->getFixtures());
+    }
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals('Ens\JobbetBundle\Controller\JobController::indexAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue($crawler->filter('.jobs td.position:contains("Expired")')->count() == 0 );
+    }
 }
